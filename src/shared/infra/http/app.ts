6f46501bc13281e70ccc8335e1eb2b60f8ec27d9
Andre Diniz <andreg.diniz97@hotmail.com>
2022-09-04 17:10:37 -0300

    Cors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,3 +1,4 @@
+import cors from "cors";
 import express, { Request, Response, NextFunction } from "express";
 import "dotenv/config";
 import "express-async-errors";
@@ -22,6 +23,7 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
 app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 
+app.use(cors());
 app.use(router);
 
 app.use(
